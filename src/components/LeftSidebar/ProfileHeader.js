d8efcdc90c1d27fbc9e87ad00ac281f24ef96536
rikeshiya <rishiriyakanikudiyil@gmail.com>
2023-12-28 18:32:00 +0530

    first commit

---
+++ src/components/LeftSidebar/ProfileHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProfileHeader() {
+  return (
+    <>
+      <div className="header">
+        <p>Riya J</p>
+      </div>
+    </>
+  );
+}
+
+export default ProfileHeader;
---
+++ src/images/UserImage.js
@@ -0,0 +1,6 @@
+export default function UserImage(){
+    const   Image={  
+      data:12,
+    
+    
+}}
